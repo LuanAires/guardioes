6b67c31133cc6ede070ce029e8c2e64541468c20
CCGstudios <130672245+LuanAires@users.noreply.github.com>
2023-10-25 21:19:29 -0300

    HUD de vida e moedas

---
+++ Assets/script/Ui_Scripts/Ui_Vidas.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ui_Vidas : MonoBehaviour
+{
+
+    private Chao Infochao;
+    private Text MeuTexto;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Infochao =GameObject.FindGameObjectWithTag("Lixeira").GetComponent<Chao>();
+        MeuTexto = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // MeuTexto.text= Infochao.vida.ToString();
+    }
+}
